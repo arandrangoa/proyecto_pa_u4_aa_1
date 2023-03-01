0d0e7cae20b5c72ace93cb520a9e421f2f3b21c6
Alex Andrango <arandrangoa@uce.edu.ec>
2023-02-28 19:44:40 -0500

    Taller 36

---
+++ src/main/java/uce/gob/ec/avanzada/proyecto_pa_u4_aa_1/ProyectoPaU4Aa1Application.java
@@ -0,0 +1,13 @@
+package uce.gob.ec.avanzada.proyecto_pa_u4_aa_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU4Aa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU4Aa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/uce/gob/ec/avanzada/proyecto_pa_u4_aa_1/ProyectoPaU4Aa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package uce.gob.ec.avanzada.proyecto_pa_u4_aa_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaU4Aa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
